1a1f8eda7dce8f4a6faae332be5403b54ebbe4b4
Vinh Quang Tran <starcraft6723@hotmail.com>
2022-10-07 22:12:49 +0700

    Change dataset path for Colab training

--- recognition/arcface_torch/configs/ms1mv2_repvgg.py
+++ recognition/arcface_torch/configs/ms1mv2_repvgg.py
@@ -14,7 +14,7 @@ config.lr = 0.1
 config.verbose = 2000
 config.dali = False
 
-config.rec = "/train_tmp/faces_emore"
+config.rec = "/content/faces_emore"
 config.num_classes = 85742
 config.num_image = 5822653
 config.num_epoch = 40
